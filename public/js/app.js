dc60a064a2f8d1dcbab1278bf04f4be3eea1e3f9
Nordov <nordov@gmail.com>
2020-04-17 00:05:31 -0700

    Trasnform size and fade out working

--- public/js/app.js
+++ public/js/app.js
@@ -3,42 +3,67 @@
 function changeHeight(element, newHeight) {
     
     let elementHeight = element.offsetHeight;
-    let resizingHeight = setInterval(resize, 5);
+    let resizingHeight = setInterval(resize, 1);
 
     function resize() {
-        if (elementHeight === newHeight) {
-        clearInterval(resizingHeight);
-        }else {
-            if (elementHeight !== newHeight) 
-                //growa ir shrinks based on ratio of current size v new size
-                element.style.height = elementHeight < newHeight ? elementHeight++ + 'px' : elementHeight-- + 'px';
-        }
+        if (elementHeight !== newHeight)
+            //growa ir shrinks based on ratio of current size v new size
+            element.style.height = elementHeight < newHeight ? elementHeight++ + 'px' : elementHeight-- + 'px';
+        else
+            clearInterval(resizingHeight);
     }
 }
 
 function changeWidth(element, newWidth) {
 
     let elementWidth = element.offsetWidth;
-    let resizingWidth = setInterval(resize, 5);
+    let resizingWidth = setInterval(resize, 1);
 
     function resize() {
-        if (elementWidth === newWidth) {
+        if (elementWidth !== newWidth)
+            //growa ir shrinks based on ratio of current size v new size
+            element.style.width = elementWidth < newWidth ? elementWidth++ + 'px' : elementWidth-- + 'px';
+        else
             clearInterval(resizingWidth);
-        } else {
-            if (elementWidth !== newWidth)
-                //growa ir shrinks based on ratio of current size v new size
-                element.style.width = elementWidth < newWidth ? elementWidth++ + 'px' : elementWidth-- + 'px';
-        }
     }
+}
+
+function fadeContent(element, fade){
+
+    if (fade === "out"){
+        let fadeEffect = setInterval(function () {
+            if (!element.style.opacity) {
+                element.style.opacity = 1;
+            }
+            if (element.style.opacity > 0) {
+                element.style.opacity -= 0.1;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 30); 
+    } else {
+        let fadeEffect = setInterval(function () {
+            if (element.style.opacity < 1) {
+                element.style.opacity += 0.1;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 30);         
+    }
+
 }
 
 const splashToNewCharacter = function() {
     const logo = document.getElementById('logo');
     const wrap = document.getElementById('wrap');
+    const wrapContent = document.getElementById('wrap-content');
+    
 
+    fadeContent(wrapContent, "out");
     changeHeight(logo, 150);
     changeHeight(wrap, 450);
     changeWidth(wrap, 900);
+    
     
  
 };
