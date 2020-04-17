1ed638369d64db776cdf5b094072a4910cd05794
Nordov <nordov@gmail.com>
2020-04-16 22:50:54 -0700

    Resize of wraps done

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 function changeHeight(element, newHeight) {
     
     let elementHeight = element.offsetHeight;
-    let resizingHeight = setInterval(resize, 10);
+    let resizingHeight = setInterval(resize, 5);
 
     function resize() {
         if (elementHeight === newHeight) {
@@ -11,7 +11,7 @@ function changeHeight(element, newHeight) {
         }else {
             if (elementHeight !== newHeight) 
                 //growa ir shrinks based on ratio of current size v new size
-                wrap.style.height = elementHeight < newHeight ? elementHeight++ + 'px' : elementHeight-- + 'px';
+                element.style.height = elementHeight < newHeight ? elementHeight++ + 'px' : elementHeight-- + 'px';
         }
     }
 }
@@ -19,7 +19,7 @@ function changeHeight(element, newHeight) {
 function changeWidth(element, newWidth) {
 
     let elementWidth = element.offsetWidth;
-    let resizingWidth = setInterval(resize, 10);
+    let resizingWidth = setInterval(resize, 5);
 
     function resize() {
         if (elementWidth === newWidth) {
@@ -27,7 +27,7 @@ function changeWidth(element, newWidth) {
         } else {
             if (elementWidth !== newWidth)
                 //growa ir shrinks based on ratio of current size v new size
-                wrap.style.width = elementWidth < newWidth ? elementWidth++ + 'px' : elementWidth-- + 'px';
+                element.style.width = elementWidth < newWidth ? elementWidth++ + 'px' : elementWidth-- + 'px';
         }
     }
 }
@@ -36,41 +36,11 @@ const splashToNewCharacter = function() {
     const logo = document.getElementById('logo');
     const wrap = document.getElementById('wrap');
 
+    changeHeight(logo, 150);
     changeHeight(wrap, 450);
     changeWidth(wrap, 900);
     
-    // const trans = function () {
-    //     wrap.animate([
-    //         { transform: 'translateX(0px)' },
-    //         { transform: 'scaleX(2)' },
-    //         //{ transform: 'translateX(300px)' }        
-    //     ], {
-    //         duration: 1000,
-    //         fill: "forwards"
-    //     });
-    // }
-
-
-    
-    
-
-    // function resize() {
-
-    //     console.log(wrapWidth);
-    //     console.log(typeof wrapWidth);
-    //     if (wrapWidth === 150 && logoHeight === 150)
-    //         clearInterval(resizing);
-    //     else {
-    //         if (wrapWidth !== 150){
-    //             wrapWidth--;
-    //             wrap.style.height = wrapWidth + 'px';
-    //         }
-    //         if (logoHeight !== 150){
-    //             logoHeight--;
-    //             logo.style.height = logoHeight.toString() + 'px';
-    //         }
-    //     }
-    // }  
+ 
 };
 
 module.exports = splashToNewCharacter;
