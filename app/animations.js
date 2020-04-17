e4297e9f51c83ddc425fa0e8a13deb49e72b2c2c
Nordov <nordov@gmail.com>
2020-04-16 21:23:46 -0700

    Height resize function

--- app/animations.js
+++ app/animations.js
@@ -2,22 +2,47 @@
 const splashToNewCharacter = function() {
     const logo = document.getElementById('logo');
     const wrap = document.getElementById('wrap');
+
+    function changeHeight(element, newSize) {
+        
+        let resizingHeight = setInterval(res, 10);
+        let elementHeight = element.offsetHeight;
+        console.log(elementHeight);
+        
+        function res() {
+            if (elementHeight === 450){
+                clearInterval(resizingHeight);
+            }else {
+                if (elementHeight !== 450) {
+                    //debugger;
+                    elementHeight < newSize ? elementHeight++ : elementHeight--;
+                    element.style.height = elementHeight + 'px';
+                }
+            }
+        }
+    }
+    
+    const trans = function () {
+        wrap.animate([
+            { transform: 'translateX(0px)' },
+            { transform: 'scaleX(2)' },
+            //{ transform: 'translateX(300px)' }        
+        ], {
+            duration: 1000,
+            fill: "forwards"
+        });
+    }
     
     window.logo = logo;
     window.wrap = wrap;
-    console.log(wrap);
+    console.log(wrap, 450);
 
     let logoHeight = logo.height;
     let wrapWidth = wrap.width;
-    let resizing = setInterval(resize, 60);
-
-    wrap.animate([
-        { transform: 'translateX(0px)' },
-        { transform: 'translateX(300px)' }
-    ], {
-        duration: 1000,
-        fill: "forwards"
-    });
+    //let resizing = setInterval(resize, 10);
+    //trans(wrap).then((wrap) => wrap.innerHTML = "<h1>Hello</h1>");
+    changeHeight(wrap);
+    
 
     function resize() {
 
