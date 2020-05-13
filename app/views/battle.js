9348df07351afd84beb78099b05a647f430221fc
Nordov <nordov@gmail.com>
2020-05-12 18:33:24 -0700

    Battle animation progress

--- app/views/battle.js
+++ app/views/battle.js
@@ -11,13 +11,21 @@ const battle = function() {
     canvas.width = 900;
     canvas.height = 600;
 
+    ctx.globalCompositeOperation = 'destination-over';
+
     let background = new Image();
     let bgNum = Math.floor(Math.random() * 5) + 1;
     background.src = "/images/bg0" + bgNum + ".png";
 
+    let fighter = new Image();
+    fighter.src = "/characters/hanzo/Idle_000.png";
+
     background.onload = function() {
         ctx.drawImage(background,0,0,900,600);
+        ctx.drawImage(fighter, 10, 10);
     }
+
+    ctx.drawImage(fighter,10,10);
 
 
 };
